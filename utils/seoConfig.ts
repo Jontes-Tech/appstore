5c7d6641a167a4718a952e4a015ede215c7069a7
Jonatan <jonatan@jontes.page>
2023-07-21 11:47:11 +0200

    Customize

--- utils/seoConfig.ts
+++ utils/seoConfig.ts
@@ -5,30 +5,21 @@ import type { ManifestOptions } from "vite-plugin-pwa"
  * Defines the default SEO configuration for the website.
  */
 export const seoConfig = {
-	baseURL: "https://example.com", // Change this to your production URL.
+	baseURL: "https://apps.nt3.me", // Change this to your production URL.
 	description:
-		"Astro PWA Starter is an opionated Astro starter for building robust static websites.", // Change this to be your website's description.
+		"Appstore with apps by Jonte.", // Change this to be your website's description.
 	type: "website",
-	image: {
-		url: "https://picsum.photos/1200/630", // Change this to your website's thumbnail.
-		alt: "OpenGraph thumbnail description.", // Change this to your website's thumbnail description.
-		width: 1200,
-		height: 630
-	},
-	siteName: "Astro PWA Starter", // Change this to your website's name,
-	twitter: {
-		card: "summary_large_image"
-	}
+	siteName: "Appstore by Jonte", // Change this to your website's name,
 }
 
 /**
  * Defines the configuration for PWA webmanifest.
  */
 export const manifest: Partial<ManifestOptions> = {
-	name: "Astro PWA Starter", // Change this to your website's name.
-	short_name: "Astro PWA Starter", // Change this to your website's short name.
+	name: "Appstore by Jonte", // Change this to your website's name.
+	short_name: "Apps", // Change this to your website's short name.
 	description:
-		"Astro PWA Starter is an opionated Astro starter for building robust static websites.", // Change this to your websites description.
+		"Download apps by Jonte", // Change this to your websites description.
 	theme_color: "#30E130", // Change this to your primary color.
 	background_color: "#ffffff", // Change this to your background color.
 	display: "minimal-ui",
